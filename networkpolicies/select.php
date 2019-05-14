4cf6bf1b7bc5a098081d71d7595e89e513a51db8
root <root@kmaster.us-central1-c.c.azurestack.internal>
2019-05-14 16:24:53 +0000

    adding demo files

---
+++ networkpolicies/select.php
@@ -0,0 +1,16 @@
+ <?php  
+ //select.php  
+ $connect = mysqli_connect("mysql", "root", "root", "db1");  
+ $output = array();  
+ $query = "SELECT * FROM tbl_user";  
+ $result = mysqli_query($connect, $query);  
+ if(mysqli_num_rows($result) > 0)  
+ {  
+      while($row = mysqli_fetch_array($result))  
+      {  
+           $output[] = $row;  
+      }  
+      echo json_encode($output);  
+ }  
+ ?>  
+
